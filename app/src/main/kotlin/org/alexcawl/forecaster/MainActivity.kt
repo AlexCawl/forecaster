be4f78302780e8d70f991bad213f05ec1b67e585
AlexCawl <babushkinsusu@gmail.com>
2024-06-26 20:59:37 +0500

    feat: added lottie animation into background

--- app/src/main/kotlin/org/alexcawl/forecaster/MainActivity.kt
+++ app/src/main/kotlin/org/alexcawl/forecaster/MainActivity.kt
@@ -1,37 +1,15 @@
 package org.alexcawl.forecaster
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import org.alexcawl.forecaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val ANIM_FADE_ENTER = 1500
-        private const val ANIM_FADE_EXIT = 2500
-    }
-
     private lateinit var binding: ActivityMainBinding
-    private lateinit var animatedBackground: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        animatedBackground = binding.root.background as AnimationDrawable
-        animatedBackground.setEnterFadeDuration(ANIM_FADE_ENTER)
-        animatedBackground.setExitFadeDuration(ANIM_FADE_EXIT)
-        animatedBackground.start()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        animatedBackground.stop()
     }
 }
